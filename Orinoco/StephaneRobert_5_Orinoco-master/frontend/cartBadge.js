5e8564d9754b1dbcfcbb28af8eb4fe47c9eb782d
Stéphane Robert <rob.steph@free.fr>
2021-08-23 23:59:30 +0200

    Delete Orinoco directory

--- Orinoco/StephaneRobert_5_Orinoco-master/frontend/cartBadge.js
+++
@@ -1,8 +0,0 @@
-// On recupere le nombre de produit présents dans le panier
-(() => {
-  const badge = document.querySelector("#cart-badge")
-  if (localStorage.getItem("cart")) {
-    let nbProduct = localStorage.getItem("cart").split(',').length
-    badge.textContent = nbProduct
-  }
-})()
